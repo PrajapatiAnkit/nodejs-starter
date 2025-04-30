a39710552436830df49c585165b06d00d52ddc68
Ankit <ankit@Ankits-MacBook-Air.local>
2025-05-01 00:44:04 +0530

    learning async patterns

---
+++ event-loop/1_setTimeout.js
@@ -0,0 +1,7 @@
+console.log("starting...");
+
+setTimeout(() => {
+  console.log("execution completed");
+}, 0);
+
+console.log("I am called 2nd");
---
+++ event-loop/2_setInterval.js
@@ -0,0 +1,7 @@
+console.log("starting...");
+
+setInterval(() => {
+  console.log("running");
+}, 2000);
+
+console.log("I am called 2nd");
